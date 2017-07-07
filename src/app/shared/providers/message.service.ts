5d45e68c81ebc841ccbf955141be69107221fbdf
Elana Kopelevich <ekopelevich@gmail.com>
2017-07-06 20:34:36 -0600

    refactor message service

--- src/app/shared/providers/message.service.ts
+++ src/app/shared/providers/message.service.ts
@@ -1,26 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http, RequestOptions } from '@angular/http';
-import { Observable } from 'Rxjs';
+import { Observable } from 'rxjs/Rx';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class MessageService {
 
-  api = 'http://localhost:3000';
-
   constructor(private http: Http) { }
 
-  public sendMessage(message) {
-    console.log('in sendMessage');
-    const url = `${this.api}/messages`;
+  public sendMessage(message): Observable<any> {
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    const options = new RequestOptions({ headers: headers });
+    const url = `${environment.baseApiUrl}/messages`;
     const msg = {
         name: message.name,
         email: message.email,
         note: message.note
       };
-    const headers = new Headers({ 'Content-Type': 'application/json' });
-    const options = new RequestOptions({ headers: headers });
-    this.http.post(url, msg).subscribe(r => {
-      console.log(r);
-    })
-    }
+    return this.http.post(url, msg);
+  }
 }
