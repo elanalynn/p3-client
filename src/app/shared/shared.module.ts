12214c123a3edb72c68af58386b085628053f884
Elana Kopelevich <ekopelevich@gmail.com>
2017-08-18 19:18:09 -0600

    update about view

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReversePipe } from './pipes/reverse.pipe';
+import { MastheadModule } from './masthead/masthead.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MastheadModule
   ],
   declarations: [ReversePipe],
   exports: [ReversePipe]
